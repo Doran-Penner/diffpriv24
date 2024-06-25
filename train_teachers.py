dfbfb73c841f7a62b245b1bbc99cd695b8355ef1
Carter Luck <cartervluck@gmail.com>
2024-06-25 11:55:28 -0700

    begin transition to pytorch

--- train_teachers.py
+++ train_teachers.py
@@ -1,102 +1,134 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+import torch
+import math
+from torch import nn, optim
+import torchvision
+torchvision.disable_beta_transforms_warning()
+import torchvision.transforms.v2 as transforms
+import numpy as np
+import torch.utils.tensorboard as tb
+import datetime
+import os
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-import deep_cnn
-import input  # pylint: disable=redefined-builtin
-import metrics
-import tensorflow.compat.v1 as tf
+#setup device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = 'cpu'
+print(device)
 
-tf.disable_eager_execution()
+#setup transform
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
 
-tf.flags.DEFINE_string('dataset', 'svhn', 'The name of the dataset to use')
-tf.flags.DEFINE_integer('nb_labels', 10, 'Number of output classes')
+dataset = torchvision.datasets.SVHN('./data/svhn', split='extra', download=True, transform=transform)
 
-tf.flags.DEFINE_string('data_dir','./saved','Temporary storage')
-tf.flags.DEFINE_string('train_dir','./saved/train_dir',
-                       'Where model ckpt are saved')
+num_teachers = 250
 
-tf.flags.DEFINE_integer('max_steps', 3000, 'Number of training steps to run.')
-tf.flags.DEFINE_integer('nb_teachers', 250, 'Teachers in the ensemble.')
-tf.flags.DEFINE_integer('teacher_id', 0, 'ID of teacher being trained.')
+train_size = int(0.7 * len(dataset))
+valid_size = int(0.2 * len(dataset))
+train_set, valid_set, test_set = torch.utils.data.random_split(dataset, [train_size, valid_size, len(dataset) - train_size - valid_size])
 
-tf.flags.DEFINE_boolean('deeper', False, 'Activate deeper CNN model')
+train_partition = [math.floor(train_size / num_teachers) + 1 for i in range(train_size % num_teachers)] + [math.floor(train_size / num_teachers) for i in range(num_teachers - (train_size % num_teachers))]
+valid_partition = [math.floor(valid_size / num_teachers) + 1 for i in range(valid_size % num_teachers)] + [math.floor(valid_size / num_teachers) for i in range(num_teachers - (valid_size % num_teachers))]
+train_sets = torch.utils.data.random_split(train_set, train_partition)
+valid_sets = torch.utils.data.random_split(valid_set, valid_partition)
 
-FLAGS = tf.flags.FLAGS
+# Normalize 
+normalize = transforms.Normalize((0.1307,), (0.3081,))
 
+# Default architecture
+arch = [(5, 64), (5, 128)]
 
-def train_teacher(dataset, nb_teachers, teacher_id):
-  """
-  This function trains a teacher (teacher id) among an ensemble of nb_teachers
-  models for the dataset specified.
-  :param dataset: string corresponding to dataset (svhn, cifar10)
-  :param nb_teachers: total number of teachers in the ensemble
-  :param teacher_id: id of the teacher being trained
-  :return: True if everything went well
-  """
-  # If working directories do not exist, create them
-  assert input.create_dir_if_needed(FLAGS.data_dir)
-  assert input.create_dir_if_needed(FLAGS.train_dir)
+class CNN(nn.Module):
 
-  # Load the dataset
-  if dataset == 'svhn':
-    train_data,train_labels,test_data,test_labels = input.ld_svhn(extended=True)
-  elif dataset == 'cifar10':
-    train_data, train_labels, test_data, test_labels = input.ld_cifar10()
-  elif dataset == 'mnist':
-    train_data, train_labels, test_data, test_labels = input.ld_mnist()
-  else:
-    print("Check value of dataset flag")
-    return False
+    def __init__(self, arch=[],padding=True):
+        super().__init__()
+        pad = 'same' if padding else 0
+        size = 32
+        layers = [nn.Conv2d(3,64,5, padding=pad)]
+        layers.append(nn.ReLU())
+        layers.append(nn.MaxPool2d(3,stride=2,padding=1))
+        layers.append(nn.LocalResponseNorm(4,alpha=0.001 / 9.0, beta=0.75))
+        
+        layers.append(nn.Conv2d(64, 128, 5, padding=pad))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(p=0.3))
+        layers.append(nn.LocalResponseNorm(4,alpha=0.001 / 9.0, beta=0.75))
+        layers.append(nn.MaxPool2d(3,stride=2,padding=1))
 
-  # Retrieve subset of data for this teacher
-  data, labels = input.partition_dataset(train_data,
-                                         train_labels,
-                                         nb_teachers,
-                                         teacher_id)
+        layers.append(nn.Flatten())
+        layers.append(nn.Linear(8192,384))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(p=0.5))
 
-  print("Length of training data: " + str(len(labels)))
+        layers.append(nn.Linear(384,192))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(p=0.5))
 
-  # Define teacher checkpoint filename and full path
-  if FLAGS.deeper:
-    filename = str(nb_teachers) + '_teachers_' + str(teacher_id) + '_deep.ckpt'
-  else:
-    filename = str(nb_teachers) + '_teachers_' + str(teacher_id) + '.ckpt'
-  ckpt_path = FLAGS.train_dir + '/' + str(dataset) + '_' + filename
+        layers.append(nn.Linear(192,10))       
 
-  # Perform teacher training
-  assert deep_cnn.train(data, labels, ckpt_path)
+        self.layers = layers
+        self.model = nn.Sequential(*layers)
 
-  # Append final step value to checkpoint for evaluation
-  ckpt_path_final = ckpt_path + '-' + str(FLAGS.max_steps - 1)
-
-  # Retrieve teacher probability estimates on the test data
-  teacher_preds = deep_cnn.softmax_preds(test_data, ckpt_path_final)
-
-  # Compute teacher accuracy
-  precision = metrics.accuracy(teacher_preds, test_labels)
-  print('Precision of teacher after training: ' + str(precision))
-
-  return True
+    def forward(self, x):
+        return self.model(x)
 
 
-def main(argv=None):  # pylint: disable=unused-argument
-  # Make a call to train_teachers with values specified in flags
-  assert train_teacher(FLAGS.dataset, FLAGS.nb_teachers, FLAGS.teacher_id)
 
-if __name__ == '__main__':
-  tf.app.run()
+def train(training_data, valid_data, arch=[], lr=1e-3, epochs=159, batch_size=16, momentum=0.9,padding=True):
+    print("training...")
+    print("training data size:",np.shape(training_data))
+    print("valid data size:",np.shape(valid_data))
+    train_loader = torch.utils.data.DataLoader(training_data, shuffle=True, batch_size=batch_size)
+    valid_loader = torch.utils.data.DataLoader(valid_data, shuffle=False, batch_size=batch_size)
+
+    network = CNN(arch=arch,padding=padding)
+    opt = optim.SGD(network.parameters(), lr=lr, momentum=momentum)
+    loss = nn.CrossEntropyLoss()
+
+    train_accs = []
+    valid_accs = []
+
+    for i in range(epochs):
+        print("Epoch",i)
+        network.train()
+        train_acc = []
+        for batch_xs, batch_ys in train_loader:
+            batch_xs = batch_xs.to(device)
+            batch_ys = batch_ys.to(device)
+
+            preds = network(normalize(batch_xs))
+            acc = (preds.argmax(dim=1) == batch_ys).float().mean()
+            train_acc.append(acc)
+
+            loss_val = loss(preds, batch_ys)
+
+            opt.zero_grad()
+            loss_val.backward()
+            opt.step()
+
+        train_accs.append(torch.tensor(train_acc).mean())
+        print("Training accuracy:",train_accs[-1])
+
+        network.eval()
+        accs = []
+        losses = []
+        for batch_xs, batch_ys in valid_loader:
+            batch_xs = batch_xs.to(device)
+            batch_ys = batch_ys.to(device)
+            preds = network(normalize(batch_xs))
+            accs.append((preds.argmax(dim=1) == batch_ys).float().mean())
+        acc = torch.tensor(accs).mean()
+        print("Valid accuracy:",acc)
+        valid_accs.append(acc)
+    return (network, valid_accs)
+
+teachers = []
+for i in range(num_teachers):
+    n, accs = train(train_sets[i],valid_sets[i],arch=arch)
+    print("TEACHER",i,"ACC",accs[-1])
+    teachers.append(n)
+    torch.save(n.state_dict(),"./saved/teacher_" + str(i) + ".txt")
