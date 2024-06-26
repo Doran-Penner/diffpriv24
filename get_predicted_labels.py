dbd38b779cfea08cbd81c98a8b812bdf9c12bf2a
Carter Luck <cartervluck@gmail.com>
2024-06-26 11:13:05 -0700

    add code to label test data

--- get_predicted_labels.py
+++ get_predicted_labels.py
@@ -1,6 +1,10 @@
 import numpy
 import torch
 from models import CNN
+import torchvision
+torchvision.disable_beta_transforms_warning()
+import torchvision.transforms.v2 as transforms
+import aggregate
 
 def getPredictedLabels(data, aggregator, num_models=250):
     votes = [] # final voting record
@@ -16,3 +20,22 @@ def getPredictedLabels(data, aggregator, num_models=250):
             ballot.append(m(p))
         votes.append(ballot)
     return aggregator.aggregate(torch.transpose(torch.Tensor(votes),0,1))
+
+#setup device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = 'cpu'
+print(device)
+
+#setup transform
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+public_dataset = torchvision.datasets.SVHN('./data/svhn', split='test', download=True, transform=transform)
+unlabeled_data = public_dataset[0][0]
+agg = aggregate.NoisyMaxAggregator(1)
+labels = getPredictedLabels(unlabeled_data,agg)
