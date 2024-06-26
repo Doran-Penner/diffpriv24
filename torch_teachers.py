2cbd59c040eb67808381afff386e8e8d3167f4d3
meesteve <118938988+meesteve@users.noreply.github.com>
2024-06-26 15:04:16 -0700

    Finished commenting and some bugfixes

--- torch_teachers.py
+++ torch_teachers.py
@@ -8,7 +8,6 @@ import numpy as np
 import torch.utils.tensorboard as tb
 import datetime
 import time
-import os
 import random
 from models import CNN
 
@@ -42,14 +41,27 @@ def load_partitioned_dataset(dataset, num_teachers):
     train_sets = torch.utils.data.random_split(dataset, train_partition, generator = torch.Generator().manual_seed(0))
     return train_sets, valid_dataset
 
-def train(training_data, valid_data, device='cpu', arch=[(5, 64), (5, 128)], lr=1e-3, epochs=70, batch_size=16, momentum=0.9,padding=True):
+def train(training_data, valid_data, dataset, device='cpu', lr=1e-3, epochs=70, batch_size=16, momentum=0.9,padding=True):
+    """
+    This is a function that trains the model on a specified dataset.
+    :param training_data: dataset containing the training data for the model
+    :param valid_data: dataset containing the validation data for the model
+    :param dataset: string containing the name of the dataset that the model is being trained on. This is to tell model.py which model to give us
+    :param device: string specifying which device the code is running on, so that the code can be appropriately optimized
+    :param lr: float specifying the learning rate for the model
+    :param epochs: int specifying the length of training
+    :param batch_size: int specifying the amount of data being trained on per batch
+    :param momentum: float specifying the momentum of the learning process. Carter says to set it at 0.9 and not worry about it
+    :param padding: boolean specifying whether we want to do padding
+    :return: Tuple containing the model being trained and the accuracy of the model on the validation set at the end of training
+    """
     print("training...")
     #print("training data size:",np.shape(training_data))
     train_loader = torch.utils.data.DataLoader(training_data, shuffle=True, batch_size=batch_size)
     valid_loader = torch.utils.data.DataLoader(valid_data, shuffle=True, batch_size=batch_size)
     
 
-    network = CNN(arch=arch,padding=padding).to(device)
+    network = CNN(padding=padding,dataset=dataset).to(device)
     opt = optim.SGD(network.parameters(), lr=lr, momentum=momentum)
     loss = nn.CrossEntropyLoss()
 
@@ -98,7 +110,13 @@ def train(training_data, valid_data, device='cpu', arch=[(5, 64), (5, 128)], lr=
     acc = torch.tensor(accs).mean()
     return (network, acc)
 
-def train_all(dataset='svhn', num_teachers=250,):
+def train_all(dataset='svhn', num_teachers=250):
+    """
+    This function trains all of the teacher models on the specified dataset
+    :param dataset: string specifying which dataset to train the teachers on
+    :param num_teachers: integer specifying the number of teachers to train
+    :return: Does not return anything, but saves the models instead
+    """
     if torch.cuda.is_available():
         device = torch.device('cuda')
     elif torch.backends.mps.is_available():
@@ -110,14 +128,14 @@ def train_all(dataset='svhn', num_teachers=250,):
     for i in range(num_teachers):
         print(f"Training teacher {i} now!")
         start_time = time.time()
-        n, acc = train(train_sets[i],valid_dataset, device)
+        n, acc = train(train_sets[i],valid_dataset,dataset,device)
         print("TEACHER",i,"ACC",acc)
-        torch.save(n.state_dict(),"./saved/teacher_" + str(i) + ".txt")
+        torch.save(n.state_dict(),f"./saved/{dataset}_teacher_{i}_of_{num_teachers}.txt")
         duration = time.time()- start_time
         print(f"It took {duration//60} minutes and {duration % 60} seconds to train teacher {i}.")
 
 def main():
-    train_all('svhn', 250)
+    train_all('mnist', 250)
 
 if __name__ == '__main__':
     main()
