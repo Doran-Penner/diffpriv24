2cb3cf22fee70b7f425650e3f9bcb06c24957b89
Doran Penner <doranaviv@gmail.com>
2024-06-26 09:30:28 -0700

    clean code

--- aggregate.py
+++ aggregate.py
@@ -1,11 +1,13 @@
 import numpy as np
 import torch
 
-def aggregate(votes, scale):
-    hist = [0]*10
+NUM_LABELS = 10  # number of labels: for SHVN (which we're hardcoding rn), it's 10
+
+def lnmax(votes, scale):
+    hist = [0]*NUM_LABELS
     for v in votes:
         hist[v] += 1
-    for label in range(10):
+    for label in range(NUM_LABELS):
         hist[label] += np.random.laplace(loc=0.0,scale=float(scale))
     label = np.argmax(hist)
     return label
@@ -17,16 +19,16 @@ def repeat_gnmax(votes, scale1, scale2, p, tau, prev_votes, prev_labels):
             U.append(voter)
     U = np.array(U)
     sub_record = votes[U]
-    hist = [0]*10
+    hist = [0]*NUM_LABELS
     for v in sub_record:
         hist[v] += 1
     seen = False
     which_record = 0
     for record in prev_votes:
-        new_hist = [0]*10
+        new_hist = [0]*NUM_LABELS
         for v in record:
             new_hist[v] += 1
-        for label in range(10):
+        for label in range(NUM_LABELS):
             hist[label] += np.random.gaussian(loc=0.0,scale=float(scale1))
         divergence = np.max(np.abs(hist-new_hist))
         if divergence < tau:
