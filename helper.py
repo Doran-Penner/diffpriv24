6130308d2fdeb1c39fad753cb90e7068e97d0c0b
meesteve <118938988+meesteve@users.noreply.github.com>
2024-07-15 14:31:39 -0700

    documentation plus cleaning some code

--- helper.py
+++ helper.py
@@ -17,6 +17,19 @@ except:  # noqa: E722
     print("using device", device)
 
 def l_inf_distances(votes, prev_votes, num_labels):
+    """
+    Function used to calculate the max difference over a label between the current
+    vote histogram and every previous vote histogram
+
+    :param votes: array of labels, where each label is the vote of a single teacher. 
+                  this list may be a subsample of the votes of every actual teacher.
+    :param prev_votes: 2-dimensional tensor variable where each prev_votes[i] looks 
+                       like the votes variable. needed to compare current votes 
+                       histogram to previous ones.
+    :param num_labels: int representing the number of labels in the dataset
+    :returns: number (maybe technically a tensor) representing the max difference
+              between vote histograms
+    """
     # using torch so we can do this on the gpu (for speed)
     hist = torch.zeros((num_labels,), device=device)
     for v in votes:
@@ -31,6 +44,20 @@ def l_inf_distances(votes, prev_votes, num_labels):
     return divergences
 
 def swing_distance(votes, prev_votes, num_labels):
+    """
+    Function used to calculate the distance between two voting records using the
+    swing voter distance metric . this is to say , the number of teachers that voted
+    for a different label given different inputs
+
+    :param votes: array of labels, where each label is the vote of a single teacher. 
+                  this list may be a subsample of the votes of every actual teacher.
+    :param prev_votes: 2-dimensional tensor variable where each prev_votes[i] looks 
+                       like the votes variable. needed to compare current votes 
+                       histogram to previous ones.
+    :param num_labels: int representing the number of labels in the dataset
+    :returns: number (maybe technically a tensor) representing the number of voters
+              that changed their vote
+    """
     swing_counts = torch.sum(prev_votes != torch.from_numpy(votes).to(device) ,dim=1)
     return swing_counts.float()
 
