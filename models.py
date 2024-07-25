c5295bf3b52e74f99dd62aaa1ede00fcabb72934
dstewtes <dstewtes@gmail.com>
2024-07-25 11:31:40 -0700

    Update models.py

--- models.py
+++ models.py
@@ -33,3 +33,39 @@ class CNN(nn.Module):
  
     def forward(self, x):
         return self.model(x)
+
+class BayesCNN(nn.module):
+
+    def __init__(self,dat_obj,dropout_iterations=100):
+        
+        # copying the same code as above but with a few differences outlined
+        # in the bayesian CNNs paper(s)
+
+        super().__init__()
+        pad = 'same'
+        size1, size2, channels = dat_obj.input_shape
+
+        # Layers:
+        # These are based on the image data active learning paper
+        # even though the Bayesian CNN intro paper has different
+        # Architecture. (this is based on the code for the paper
+        # not the paper actual stuff)
+        layers = [nn.Conv2d(channels,64,5,padding=pad)]
+        layers.append(nn.ReLU())
+        layers = [nn.Conv2d(64,128,5,padding=pad)]
+        layers.append(nn.ReLU())
+        layers.append(nn.MaxPool2d(3,stride=2,padding=1))
+        layers.append(nn.Dropout(p=0.25))
+
+        layers.append(nn.Flatten())
+        layers.append(nn.Linear(size1*size2*32,256)) # add optimizer for regularization?
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(p=0.5))
+        layers.append(nn.Linear(256,dat_obj.num_labels))
+        layers.append(nn.Softmax())
+
+        self.layers = layers
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x): # taken from normal CNN bit
+        return self.model(x)
