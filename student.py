d31ad6b89aa255c5be8b558c0f6d7e510b018687
dstewtes <dstewtes@gmail.com>
2024-07-30 14:23:38 -0700

    Update student.py

--- student.py
+++ student.py
@@ -135,7 +135,7 @@ def active_learning(network=BBB3Conv3FC,acquisition_iterations=10,initial_size=1
 
     # saving relevant information for later (mainly for testing purposes)
     test_dict["valid_loss"].append(valid_loss)
-    test_dict["epsilon"].append(gnmax_epsilon(data_dep_eps_costs,sigma=40))
+    test_dict["epsilon"].append(gnmax_epsilon(data_dep_eps_costs,alpha=agg.alpha,sigma=agg.scale,delta=1e6))
     test_dict["test_acc"].append(calculate_test_accuracy(model,dat_obj.student_test))
     
     # size of the subset of the pool we wish to acquire from
@@ -165,7 +165,7 @@ def active_learning(network=BBB3Conv3FC,acquisition_iterations=10,initial_size=1
 
         test_dict["valid_loss"].append(valid_loss)
         test_dict["test_acc"].append(calculate_test_accuracy(model,dat_obj.student_test))
-        test_dict["epsilon"].append(gnmax_epsilon(data_dep_eps_costs)) # TODO possibly change this for abstraction later!
+        test_dict["epsilon"].append(gnmax_epsilon(data_dep_eps_costs,alpha=agg.alpha,sigma=agg.scale,delta=1e6)) # TODO possibly change this for abstraction later!
 
     if print_summary:
         print_assessment(test_dict,initial_size,acquisition_iterations,num_acquisitions)
