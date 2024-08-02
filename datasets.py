8df7478e9dab9a9c7e9110d001403bc2c5caf6bc
dstewtes <dstewtes@gmail.com>
2024-08-02 11:30:57 -0700

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -171,8 +171,8 @@ class _MNIST(_Dataset):
             download=True,
             transform=self._transform
         )
-        og_train.target = self.one_hot(og_train.target)
-        og_test.target = self.one_hot(og_test.target)
+        og_train.targets = self.one_hot(og_train.targets)
+        og_test.targets = self.one_hot(og_test.targets)
 
         all_teach_train, all_teach_valid = torch.utils.data.random_split(
             og_train,
