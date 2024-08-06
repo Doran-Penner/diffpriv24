5e2cf3e7b36703fe983dd6867bd198d507fb7727
dstewtes <dstewtes@gmail.com>
2024-08-06 16:05:32 -0700

    Update BASS_main.py

--- BASS_main.py
+++ BASS_main.py
@@ -92,7 +92,7 @@ def acquire_balanced_init(indices,data_object,n_per_label):
     i = 0
     while len(removed_inds) < n_labels * n_per_label:
 
-        possible = data_object.student_data.dataset.targets[indices[i]]
+        possible = torch.argmax(data_object.student_data.dataset.targets[indices[i]]).item()
         if labels_counts[possible] < n_per_label:
             labels_counts[possible] += 1
             removed_inds.append(indices[i])
